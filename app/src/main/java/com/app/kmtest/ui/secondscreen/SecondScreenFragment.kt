283d7d27d1a4f3f5d5d59462bd78299907e06df4
Ichsanul Alifwan <ichsanulalifwan@gmail.com>
2022-01-11 20:35:49 +0800

    implement second screen features

--- app/src/main/java/com/app/kmtest/ui/secondscreen/SecondScreenFragment.kt
+++ app/src/main/java/com/app/kmtest/ui/secondscreen/SecondScreenFragment.kt
@@ -1,19 +1,72 @@
 package com.app.kmtest.ui.secondscreen
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.app.kmtest.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.app.kmtest.databinding.FragmentSecondScreenBinding
 
 class SecondScreenFragment : Fragment() {
+
+    private var _binding: FragmentSecondScreenBinding? = null
+    private val binding get() = _binding!!
+    private val args by navArgs<SecondScreenFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second_screen, container, false)
+        _binding = FragmentSecondScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (activity != null) {
+
+            // Init Toolbar
+            val toolbar = binding.toolbar
+
+            // Set Navigate to previous page (backstack)
+            toolbar.setNavigationOnClickListener {
+                it.findNavController().navigateUp()
+            }
+
+            // Get Data
+            val userName = args.userName
+            val selectedUser = args.selectedUser
+
+            // Populate Data
+            populateData(userName, selectedUser)
+
+            // Navigate to Third Screen
+            binding.btnChooseUser.setOnClickListener {
+                val actionToThird =
+                    SecondScreenFragmentDirections.actionSecondScreenFragmentToThirdScreenFragment(
+                        userName
+                    )
+                findNavController().navigate(actionToThird)
+            }
+        }
+    }
+
+    // Populate and set data from previous page
+    private fun populateData(userName: String?, selectedUser: String?) {
+        with(binding) {
+            if (userName != null && userName.isNotEmpty()) tvUserName.text = userName
+            if (selectedUser != null && selectedUser.isNotEmpty()) tvSelectedUser.text =
+                selectedUser
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
