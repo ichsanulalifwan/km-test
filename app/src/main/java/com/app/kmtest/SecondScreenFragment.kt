094497bed79ae649294885758d47f2aa0b65f64b
Ichsanul Alifwan <ichsanulalifwan@gmail.com>
2022-01-11 16:44:29 +0800

    add fragments

---
+++ app/src/main/java/com/app/kmtest/SecondScreenFragment.kt
@@ -0,0 +1,18 @@
+package com.app.kmtest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SecondScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second_screen, container, false)
+    }
+}
