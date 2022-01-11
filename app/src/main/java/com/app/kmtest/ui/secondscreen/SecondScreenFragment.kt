096c7b87d06bf655275085b2ef58ed1749798a32
Ichsanul Alifwan <ichsanulalifwan@gmail.com>
2022-01-11 18:57:11 +0800

    change project structure

--- app/src/main/java/com/app/kmtest/ui/secondscreen/SecondScreenFragment.kt
+++ app/src/main/java/com/app/kmtest/ui/secondscreen/SecondScreenFragment.kt
@@ -1,10 +1,11 @@
-package com.app.kmtest
+package com.app.kmtest.ui.secondscreen
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.app.kmtest.R
 
 class SecondScreenFragment : Fragment() {
 
--- app/src/main/java/com/app/kmtest/ui/thirdscreen/ThirdScreenFragment.kt
+++ app/src/main/java/com/app/kmtest/ui/thirdscreen/ThirdScreenFragment.kt
@@ -1,18 +1,19 @@
-package com.app.kmtest
+package com.app.kmtest.ui.thirdscreen
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.app.kmtest.R
 
-class FirstScreenFragment : Fragment() {
+class ThirdScreenFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_screen, container, false)
+        return inflater.inflate(R.layout.fragment_third_screen, container, false)
     }
 }
